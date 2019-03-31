9dedffdea649c95dd726738943c25d81900e70a7
sappp <assaf.attia@gmail.com>
2019-03-31 11:31:31 +0300

    a

--- src/index.js
+++ src/index.js
@@ -3,22 +3,12 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import "bootstrap-css-only";
 import configureStore from "./state/configureStore";
-// import actions from './state/actions';
+
 import App from "./view/App";
 
 const {store, actions} = configureStore({});
-// debug stuff
-Object.defineProperty(window, "state", { get: store.getState });
-console.log(actions);
-// window.selectors = selectors;
-// window.actions = store.actions;
-// window.constants = store.constants;
-console.log('********************************************* \n DEVELOPMENT MODE \n window.state available \n window.selectors available \n ********************************************* \n');
+
 actions.movies.fetchMoviesOperation('batman');
-// actions.movies.fetchDetailsOperation();
-// moviesApi.getMovies("a").then(res => {
-//   console.log(res);
-// });
 
 ReactDOM.render(
   <Provider store={store}>
